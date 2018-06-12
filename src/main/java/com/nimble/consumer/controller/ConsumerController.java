47d50305f592a15443e0d71f8f859543132ca1ff
yanqiuhuang188 <yanqiuhuang188@163.com>
2018-06-13 00:31:31 +0800

    update

--- src/main/java/com/nimble/consumer/controller/ConsumerController.java
+++ src/main/java/com/nimble/consumer/controller/ConsumerController.java
@@ -4,11 +4,15 @@ package com.nimble.consumer.controller;
 import com.nimble.consumer.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class ConsumerController {
@@ -18,6 +22,9 @@ public class ConsumerController {
 
     @Autowired
     private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    DiscoveryClient discoveryClient;
 
     @GetMapping("/consume/{id}")
     public User findById(@PathVariable Long id){
@@ -30,6 +37,33 @@ public class ConsumerController {
     public String microServiceNodeInfo(){
         ServiceInstance serviceInstance = this.loadBalancerClient.choose("provider");
         return serviceInstance.toString();
+    }
+
+    @RequestMapping("/registered")
+    public String getRegistered(){
+        List<ServiceInstance> list = discoveryClient.getInstances("PROVIDER");
+        System.out.println(discoveryClient.getServices());
+        System.out.println("discoveryClient.getServices().size() = " + discoveryClient.getServices().size());
+
+        for( String s :  discoveryClient.getServices()){
+            System.out.println("services " + s);
+            List<ServiceInstance> serviceInstances =  discoveryClient.getInstances(s);
+            for(ServiceInstance si : serviceInstances){
+                System.out.println("    services:" + s + ":getHost()=" + si.getHost());
+                System.out.println("    services:" + s + ":getPort()=" + si.getPort());
+                System.out.println("    services:" + s + ":getServiceId()=" + si.getServiceId());
+                System.out.println("    services:" + s + ":getUri()=" + si.getUri());
+                System.out.println("    services:" + s + ":getMetadata()=" + si.getMetadata());
+            }
+
+        }
+
+        if (list != null && list.size() > 0 ) {
+            for(ServiceInstance instance : list){
+                System.out.println(instance.getUri());
+            }
+        }
+        return null;
     }
 
 }
